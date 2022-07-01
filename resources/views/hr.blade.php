c625cce83282be674cedf0b532523302ce75ebc2
florifrrokaj <florifrrokaj00@gmail.com>
2022-07-01 10:58:38 +0200

    HR changes

--- resources/views/hr.blade.php
+++ resources/views/hr.blade.php
@@ -24,7 +24,6 @@
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap');
         @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Poppins:wght@200;800;900&display=swap');
-
     </style>
 </head>
 
@@ -2105,7 +2104,7 @@
                                                                 <div class="">
                                                                     @if ($admins[$i]->admin_id == null)
                                                                         <button onclick="window.location.href='{{route('employeProfile',$admins[$i]->id)}}'" class="adminViewProfileBtn w-100 py-1">Profil Anzeigen</button>
-     
+    
                                                                     @else
                                                                         <button onclick="window.location.href='{{route('employeProfile',$admins[$i]->headadmin->id)}}'" class="adminViewProfileBtn w-100 py-1">Profil Anzeigen</button>
                                                                     @endif
