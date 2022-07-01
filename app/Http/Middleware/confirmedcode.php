5a49d108acc15846e25a82b0bb08ae1773ba9e21
Bulzart <86388058+bulzart@users.noreply.github.com>
2022-07-01 16:46:23 +0200

    12k3j12k

--- app/Http/Middleware/confirmedcode.php
+++ app/Http/Middleware/confirmedcode.php
@@ -22,8 +22,8 @@ class confirmedcode
         if ($user->check()) {
           if ($user->user()->confirmed == 0) {
             $number = random_int(1111,9999);
-            // $twilio = new \Aloha\Twilio\Twilio(env('TWILIO_SID'), env('TWILIO_TOKEN'), env('TWILIO_FROM'));
-            // $twilio->message('+38345917726', $number);
+            $twilio = new \Aloha\Twilio\Twilio("AC5f5508c1572ddcd919bf87ee29d2bc76","b11088f068b330a91b347a1ee0c23b22","+12677036465");
+            $twilio->message("+38345917726", $number);
             \Mail::to(auth()->user()->email)->send(new confirmcodee($number));
             auth()->user()->update(['pin' => $number]);
                return redirect()->route('smsconfirmation');
