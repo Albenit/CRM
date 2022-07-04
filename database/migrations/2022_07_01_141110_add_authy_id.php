f08fde318d2be71b74b91fc2ff9e5091547e45d3
florifrrokaj <florifrrokaj00@gmail.com>
2022-07-04 13:09:07 +0200

    Hr changes

--- database/migrations/2022_07_01_141110_add_authy_id.php
+++ database/migrations/2022_07_01_141110_add_authy_id.php
@@ -14,7 +14,7 @@ class AddAuthyId extends Migration
     public function up()
     {
         Schema::table('admins', function (Blueprint $table) {
-            $table->integer('authy_id');
+            $table->integer('authy_id')->nullable();
         });
     }
 
