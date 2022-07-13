26a1362574dbcac47363ae879eb3e3eac93c85e7
Bulzart <86388058+bulzart@users.noreply.github.com>
2022-07-13 14:49:06 +0200

    1;23j12k3j12

--- resources/views/costumers.blade.php
+++ resources/views/costumers.blade.php
@@ -816,6 +816,7 @@
                             </div>
                         </div>
                     </div>
+                    @if(!auth()->user()->hasRole('fs'))
                     <div class="d-flex justify-content-end py-1 pe-3" style="background-color: transparent;">
                         <div class="prev-nxt-btn d-flex">
                             <a href="{{route('costumers',['page' => $data->currentPagee - 1])}}">
@@ -836,6 +837,28 @@
                             @endif
                         </div>
                     </div>
+                    @else
+                    <div class="d-flex justify-content-end py-1 pe-3" style="background-color: transparent;">
+                        <div class="prev-nxt-btn d-flex">
+                            <a href="{{route('costumers',['page' => $data->currentPage() - 1])}}">
+                                <div class="prev-btn border p-2 bg-light m-2 rounded">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+                                    </svg>
+                                </div>
+                            </a>
+                            @if($data->count() > 0)
+                                <a href="{{route('costumers',['page' => $data->currentPage() + 1])}}">
+                                    <div class="nxt-btn border p-2 bg-light m-2 rounded">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
+                                            <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+                                        </svg>
+                                    </div>
+                                </a>
+                            @endif
+                        </div>
+                    </div>
+                    @endif
                 </div>
                 @if(auth()->user()->hasRole('admin') || auth()->user()->hasRole('backoffice'))
                 <div class="pt-4">
